8e3a559c7ae26a2c1d76018133f9c8aae102b665
URSGOWRI <gowr.u.urs@gmail.com>
2023-09-11 15:55:09 +0530

    URSGOWRI first commit

---
+++ src/tets.java
@@ -0,0 +1,11 @@
+
+public class tets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("TRYING TO EXPLORE TO GIT");
+System.out.println("We will also exlore local repo and remote repo");
+System.out.println("We will also exlore gitbash and github");
+	}
+
+}
